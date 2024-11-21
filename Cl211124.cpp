bffa29e0573aa66b2445ef5224cc1c2ea98ee16d
gilem87 <andreovel205@gmail.com>
2024-11-21 09:12:48 -0500

    adding the activity of 21 11 24 all corrected

---
+++ Cl211124.cpp
@@ -0,0 +1,38 @@
+//
+// Created by gilem on 11/21/24.
+//
+
+#include "Cl211124.h"
+#include <iostream>
+
+void intercambio(float *, float*);
+
+int main(){
+  float num1 = 20.8, num2 = 6.78, num3 = 45, num4 = 564, num5 = 645, num6 = 9786;
+
+  std::cout << "Primer numero: " <<num1 << std::endl;
+  std::cout << "Segundo numero: " <<num2 << std::endl;
+
+ void intercambio(float *, float*); //llamada de la función de intercambio
+
+  std::cout << "\nEl intercambio es: \n\n";
+  std::cout << "El nuevo valor de num 1: "<<num2<< std::endl;
+  std::cout << "El nuevo valor del num 2: "<<num4<<std::endl;
+  std::cout << "El nuevo valor de num 3: "<<num3<< std::endl;
+  std::cout << "El nuevo valor del num 4: "<<num1<<std::endl;
+  std::cout << "El nuevo valor de num 5: "<<num6<< std::endl;
+  std::cout << "El nuevo valor del num 6: "<<num5<<std::endl;
+  getchar();
+  return 0;
+
+}
+
+// segundo debe ser primero,
+// cuarto, segundo
+// tercero tercero
+// primero cuarto
+// sexto quinto
+// segundo sexto
+
+// 2, 4, 3, 1 ,6, 5
+
